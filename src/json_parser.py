ea2bad3da3f851b8cdf8ce77a7eec7791e205723
Kit <kittsville@googlemail.com>
2016-10-13 22:03:43 +0100

    Removed deprecated JSON parser wrapper

--- src/json_parser.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def json_parser(response):
-    try:
-        json_response = response.json()
-        
-        return json_response
-    except ValueError:
-        return False
